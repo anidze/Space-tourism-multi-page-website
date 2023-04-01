f1ff50e76fc5dbf95b8b9c76654ed970ed26b28c
ano gambashidze <ano.gambashidze@gmail.com>
2023-04-02 03:12:46 +0400

    final version of mobile device

---
+++ project.js
@@ -0,0 +1,5 @@
+// let rectangle=document.createElement("div");
+// rectangle.classList.add('crewlineStyle');
+// console.log(rectangle)
+// const imgs=document.querySelector('.crewImg')
+// imgs.appendChild(rectangle)
